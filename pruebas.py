378c0b119523bfdc491b05090c06b386904afa1b
cquipu <edsonj8@hotmail.com>
2026-03-15 21:27:18 -0500

    sesion 6

---
+++ pruebas.py
@@ -0,0 +1,5 @@
+nombre = input("Ingrese su nombre: ")
+year = int(input("Ingrese su año de nacimiento: "))
+
+print("nombre:", nombre, "Edad:", 2026-year, sep="--", end="##$$")
+#print(f"Nombre:{nombre} Edad: {2026-year}")
